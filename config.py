60c4e23b4c4a8961a1c77b00a50b20cdf151cd5f
Agnibho Mondal <mondal@agnibho.com>
2023-09-03 01:09:31 +0530

    Accept prescriber as argument

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ import argparse, json, os
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", nargs="?")
 parser.add_argument("-c", "--config")
+parser.add_argument("-p", "--prescriber")
 args = parser.parse_args()
 
 if(args.config is None):
@@ -33,4 +34,13 @@ with open(config_file) as conf:
 config = default | read
 config["filename"]=args.filename
 config["template"]=os.path.join(config["template_directory"], config["template"])
-config["prescriber"]=os.path.join(config["config_directory"], config["prescriber"])
+if(args.prescriber is None):
+    config["prescriber"]=os.path.join(config["config_directory"], config["prescriber"])
+else:
+    if(not os.path.isabs(args.prescriber)):
+        args.prescriber=os.path.join(config["config_directory"], args.prescriber)
+    if(os.path.isfile(args.prescriber)):
+        config["prescriber"]=args.prescriber
+    else:
+        config["prescriber"]=os.path.join(config["config_directory"], config["prescriber"])
+        print("File "+args.prescriber+" not found.")
